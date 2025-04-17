e56dc648f01da6fd16cd1a8a67645d078281cfc7
Rafael-Cerqueira <rafael.scerqueira@hotmail.com>
2025-04-16 22:31:10 -0300

    Fix: Schema DoctorScheduleRequest + HealthcareFacilityRequest

--- facility-service/src/main/java/br/com/fiap/tech/facility/dto/DoctorScheduleRequest.java
+++ facility-service/src/main/java/br/com/fiap/tech/facility/dto/DoctorScheduleRequest.java
@@ -1,17 +1,31 @@
 package br.com.fiap.tech.facility.dto;
 
 import br.com.fiap.tech.facility.domain.DayOfWeek;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 
 import java.time.LocalTime;
 
 @Data
 public class DoctorScheduleRequest {
+    @Schema(example = "MONDAY", description = "Dia da semana para o agendamento")
     private DayOfWeek dayOfWeek;
+    
+    @Schema(example = "09:00:00", description = "Horário de início do primeiro período (formato HH:MM:SS)")
     private LocalTime startTime;
+    
+    @Schema(example = "12:00:00", description = "Horário de término do primeiro período (formato HH:MM:SS)")
     private LocalTime endTime;
+    
+    @Schema(example = "14:00:00", description = "Horário de início do segundo período, se aplicável (formato HH:MM:SS)")
     private LocalTime secondPeriodStart;
+    
+    @Schema(example = "18:00:00", description = "Horário de término do segundo período, se aplicável (formato HH:MM:SS)")
     private LocalTime secondPeriodEnd;
+    
+    @Schema(example = "1", description = "ID do médico")
     private Long doctorId;
+    
+    @Schema(example = "1", description = "ID da unidade de saúde")
     private Long facilityId;
 }
--- facility-service/src/main/java/br/com/fiap/tech/facility/dto/HealthcareFacilityRequest.java
+++ facility-service/src/main/java/br/com/fiap/tech/facility/dto/HealthcareFacilityRequest.java
@@ -1,19 +1,41 @@
 package br.com.fiap.tech.facility.dto;
 
 import br.com.fiap.tech.facility.domain.FacilityType;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 
 @Data
 public class HealthcareFacilityRequest {
+    @Schema(example = "Hospital São Luiz")
     private String name;
+    
+    @Schema(example = "HOSPITAL")
     private FacilityType facilityType;
+    
+    @Schema(example = "12345678901234")
     private String cnpj;
+    
+    @Schema(example = "(11) 98765-4321")
     private String phoneNumber;
+    
+    @Schema(example = "Av. Paulista, 1000")
     private String address;
+    
+    @Schema(example = "São Paulo")
     private String city;
+    
+    @Schema(example = "SP")
     private String state;
+    
+    @Schema(example = "01310100")
     private String zipCode;
+    
+    @Schema(example = "-23.5505")
     private Double latitude;
+    
+    @Schema(example = "-46.6333")
     private Double longitude;
+    
+    @Schema(example = "100")
     private Integer maxDailyCapacity;
 }
