ecdfc32e65cb8e77c0c4cf004ef1e5f325bcf31a
Rafael-Cerqueira <rafael.scerqueira@hotmail.com>
2025-04-17 22:35:59 -0300

    delete src

--- src/main/java/br/com/fiap/tech/scheduling/config/OpenFeignConfig.java
+++
@@ -1,8 +0,0 @@
-package br.com.fiap.tech.scheduling.config;
-
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-public class OpenFeignConfig {
-    // Esta classe foi modificada para evitar conflitos
-} 
